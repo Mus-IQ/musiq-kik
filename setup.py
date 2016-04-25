638b0ebfe61f78cc79bc9cc713cf56df388e1131
Marwan Ahmed <marwanad@users.noreply.github.com>
2016-04-25 18:09:31 -0400

    new token env vars

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ kik = KikApi(bot_config["username"], bot_config["key"])
 
 
 def get_spotify_token():
-    cached_token = os.environ.get('SPOTIPY_ACCESS_TOKEN')
+    cached_token = os.environ.get('MUSIC_ACCESS_TOKEN')
     # if token is valid, no need to request a new one
     if (is_cached_token_valid()):
         print("Returning a cached token since it's valid")
@@ -26,10 +26,10 @@ def get_spotify_token():
     print("Requesting a new token!")
     token_data = {
         'grant_type': 'refresh_token',
-        'refresh_token': os.environ.get('SPOTIPY_REFRESH_TOKEN'),
+        'refresh_token': os.environ.get('MUSIC_REFRESH_TOKEN'),
     }
 
-    clientID = os.environ.get('SPOTIPY_CLIENT_ID') + ":" + os.environ.get('SPOTIPY_CLIENT_SECRET')
+    clientID = os.environ.get('MUSIC_CLIENT_ID') + ":" + os.environ.get('MUSIC_CLIENT_SECRET')
     b64Val = base64.b64encode(clientID)
     r = requests.post('https://accounts.spotify.com/api/token', headers={'Authorization': 'Basic ' + b64Val},
                       data=token_data)
@@ -37,13 +37,13 @@ def get_spotify_token():
     token_response_json = r.json()
     # get token from response and store
     access_token = token_response_json['access_token']
-    os.environ["SPOTIPY_ACCESS_TOKEN"] = access_token
+    os.environ["MUSIC_ACCESS_TOKEN"] = access_token
     print("Got a new token with: ", access_token)
 
     return access_token
 
 def is_cached_token_valid():
-    cached_token = os.environ.get('SPOTIPY_ACCESS_TOKEN')
+    cached_token = os.environ.get('MUSIC_ACCESS_TOKEN')
     print("Cached token is: ", cached_token)
 
     r = requests.get('https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6', headers={'Authorization': 'Bearer ' + cached_token})
